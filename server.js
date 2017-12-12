66cbee9f015dda2ccd3936244fefc6aeb927713f
ivgi84 <ivgi84@gmail.com>
2017-12-12 08:23:16 +0200

    update

--- server.js
+++ server.js
@@ -9,9 +9,23 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next){
+    console.log('request cought by middlewear');
+    next();
+});
+
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
+
+router.get('/test', function(req, res){
+    res.json({message:'this is a test'});
+})
+
+router.route('/createImage')
+    .post(function(req, res){
+        res.json({message:'post request to create an image'});
+    });
 
 // all of our routes will be prefixed with /api
 app.use('/api', router);
