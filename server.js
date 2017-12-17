b8bdca2c1d5754c59c6dd3953812a74939ea935a
Evgeny Smolovich <evgenysmo@etoro.com>
2017-12-17 18:18:29 +0200

    added nodemon

--- server.js
+++ server.js
@@ -5,30 +5,38 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 4201;
 
 var router = express.Router();
 
-router.use(function(req, res, next){
-    console.log('request cought by middlewear');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+router.use(function(req, res, next) {
+    console.log('request cought by middlewear');
+    next();
 });
-
-router.get('/test', function(req, res){
-    res.json({message:'this is a test'});
-})
-
-router.route('/createImage')
-    .post(function(req, res){
-        res.json({message:'post request to create an image'});
-    });
-
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+router.get('/test', function(req, res) {
+    res.json({ message: 'this is a test' });
+});
+
+router.route('/createImage')
+    .post(function(req, res) {
+        //todo: add canvas to image library
+        res.json({ message: 'post request to create an image' });
+    });
+
+
 
 
 app.listen(port);
